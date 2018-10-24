ddd79738f9dab12ac791df509df868c39c562251
Renan <renancaraujo@users.noreply.github.com>
2018-10-24 09:12:18 -0300

    Update babel, add patch and create build for es and cjs (#2)
    * adding patch
    
    * update readme
    
    * add npm badge
    
    * add patch
    
    * update
    
    * Upgrade babel & update build
    
    * Fix to right version

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,44 @@
 import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import pkg from './package.json';
 
-export default {
-  input: 'src/index.js',
-  output: [
-    {
-      file: 'dist/umd/index.js',
-      format: 'umd',
-      indent: '  ',
-      name: 'axiosEndpoints',
-    },
-  ],
-  plugins: [babel()],
-};
+const extensions = ['.ts', '.tsx', '.js', '.jsx', '.json'];
+
+export default [
+  {
+    experimentalCodeSplitting: true,
+    input: 'src/index.js',
+    output: [
+      {
+        format: 'es',
+        dir: 'dist/es',
+      },
+      {
+        format: 'cjs',
+        dir: 'dist/lib',
+        exports: 'named',
+      },
+    ],
+    external: ["axios"],
+    plugins: [
+      babel({
+        extensions,
+        exclude: 'node_modules/**',
+        externalHelpers: true,
+      }),
+      resolve(),
+    ],
+  },
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        file: 'dist/umd/index.js',
+        format: 'umd',
+        indent: '  ',
+        name: 'axiosEndpoints',
+      },
+    ],
+    plugins: [babel({ externalHelpers: true })],
+  },
+];
