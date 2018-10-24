071e30647b24a2614fdffb3393327d7783c61336
Renan <renan.araujo@dextra-sw.com>
2018-10-24 10:11:50 -0300

    fix tests

--- src/EndpointFactory.js
+++ src/EndpointFactory.js
@@ -60,16 +60,5 @@ export default function EndpointFactory(axiosInstance) {
         ...endpointOptions,
       });
     };
-    patch = (payload = {}, options = {}) => {
-      const { params = {}, uriParams = {}, ...endpointOptions } = options;
-      return axiosInstance({
-        method: 'put',
-        url: this.uri || this.uriFunction({ ...uriParams }),
-        data: payload,
-        params,
-        responseType: 'json',
-        ...endpointOptions,
-      });
-    };
   };
 }
