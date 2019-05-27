498b51b77b23a9b78084fc693573e150bad4d39e
Renan Araujo <renan.araujo@dextra-sw.com>
2019-05-27 16:41:36 -0300

    dist after audit

--- dist/lib/index.js
+++ dist/lib/index.js
@@ -6,173 +6,84 @@ function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'defau
 
 var axios = _interopDefault(require('axios'));
 
-function _classCallCheck(instance, Constructor) {
-  if (!(instance instanceof Constructor)) {
-    throw new TypeError("Cannot call a class as a function");
-  }
-}
-
-function _defineProperty(obj, key, value) {
-  if (key in obj) {
-    Object.defineProperty(obj, key, {
-      value: value,
-      enumerable: true,
-      configurable: true,
-      writable: true
-    });
-  } else {
-    obj[key] = value;
-  }
-
-  return obj;
-}
-
-function _objectSpread(target) {
-  for (var i = 1; i < arguments.length; i++) {
-    var source = arguments[i] != null ? arguments[i] : {};
-    var ownKeys = Object.keys(source);
-
-    if (typeof Object.getOwnPropertySymbols === 'function') {
-      ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) {
-        return Object.getOwnPropertyDescriptor(source, sym).enumerable;
-      }));
-    }
-
-    ownKeys.forEach(function (key) {
-      _defineProperty(target, key, source[key]);
-    });
-  }
-
-  return target;
-}
-
-function _objectWithoutPropertiesLoose(source, excluded) {
-  if (source == null) return {};
-  var target = {};
-  var sourceKeys = Object.keys(source);
-  var key, i;
-
-  for (i = 0; i < sourceKeys.length; i++) {
-    key = sourceKeys[i];
-    if (excluded.indexOf(key) >= 0) continue;
-    target[key] = source[key];
-  }
-
-  return target;
-}
-
-function _objectWithoutProperties(source, excluded) {
-  if (source == null) return {};
-
-  var target = _objectWithoutPropertiesLoose(source, excluded);
-
-  var key, i;
-
-  if (Object.getOwnPropertySymbols) {
-    var sourceSymbolKeys = Object.getOwnPropertySymbols(source);
-
-    for (i = 0; i < sourceSymbolKeys.length; i++) {
-      key = sourceSymbolKeys[i];
-      if (excluded.indexOf(key) >= 0) continue;
-      if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue;
-      target[key] = source[key];
-    }
-  }
-
-  return target;
-}
-
 function EndpointFactory(axiosInstance) {
-  return function ApiClient(endpoint, _endpointOptions) {
+  var _temp;
+
+  return _temp = function ApiClient(endpoint, _endpointOptions) {
     var _this = this;
 
-    _classCallCheck(this, ApiClient);
-
-    _defineProperty(this, "get", function () {
+    babelHelpers.classCallCheck(this, ApiClient);
+    babelHelpers.defineProperty(this, "get", function () {
       var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-
       var _options$params = options.params,
           params = _options$params === void 0 ? {} : _options$params,
           _options$uriParams = options.uriParams,
           uriParams = _options$uriParams === void 0 ? {} : _options$uriParams,
-          endpointOptions = _objectWithoutProperties(options, ["params", "uriParams"]);
-
-      return axiosInstance(_objectSpread({
-        url: _this.uri || _this.uriFunction(_objectSpread({}, uriParams)),
+          endpointOptions = babelHelpers.objectWithoutProperties(options, ["params", "uriParams"]);
+      return axiosInstance(babelHelpers.objectSpread({
+        url: _this.uri || _this.uriFunction(babelHelpers.objectSpread({}, uriParams)),
         params: params,
         responseType: 'json'
       }, endpointOptions));
     });
-
-    _defineProperty(this, "post", function () {
+    babelHelpers.defineProperty(this, "post", function () {
       var payload = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
       var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
-
       var _options$params2 = options.params,
           params = _options$params2 === void 0 ? {} : _options$params2,
           _options$uriParams2 = options.uriParams,
           uriParams = _options$uriParams2 === void 0 ? {} : _options$uriParams2,
-          endpointOptions = _objectWithoutProperties(options, ["params", "uriParams"]);
-
-      return axiosInstance(_objectSpread({
+          endpointOptions = babelHelpers.objectWithoutProperties(options, ["params", "uriParams"]);
+      return axiosInstance(babelHelpers.objectSpread({
         method: 'post',
-        url: _this.uri || _this.uriFunction(_objectSpread({}, uriParams)),
+        url: _this.uri || _this.uriFunction(babelHelpers.objectSpread({}, uriParams)),
         data: payload,
         params: params,
         responseType: 'json'
       }, endpointOptions));
     });
-
-    _defineProperty(this, "put", function () {
+    babelHelpers.defineProperty(this, "put", function () {
       var payload = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
       var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
-
       var _options$params3 = options.params,
           params = _options$params3 === void 0 ? {} : _options$params3,
           _options$uriParams3 = options.uriParams,
           uriParams = _options$uriParams3 === void 0 ? {} : _options$uriParams3,
-          endpointOptions = _objectWithoutProperties(options, ["params", "uriParams"]);
-
-      return axiosInstance(_objectSpread({
+          endpointOptions = babelHelpers.objectWithoutProperties(options, ["params", "uriParams"]);
+      return axiosInstance(babelHelpers.objectSpread({
         method: 'put',
-        url: _this.uri || _this.uriFunction(_objectSpread({}, uriParams)),
+        url: _this.uri || _this.uriFunction(babelHelpers.objectSpread({}, uriParams)),
         data: payload,
         params: params,
         responseType: 'json'
       }, endpointOptions));
     });
-
-    _defineProperty(this, "patch", function () {
+    babelHelpers.defineProperty(this, "patch", function () {
       var payload = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
       var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
-
       var _options$params4 = options.params,
           params = _options$params4 === void 0 ? {} : _options$params4,
           _options$uriParams4 = options.uriParams,
           uriParams = _options$uriParams4 === void 0 ? {} : _options$uriParams4,
-          endpointOptions = _objectWithoutProperties(options, ["params", "uriParams"]);
-
-      return axiosInstance(_objectSpread({
+          endpointOptions = babelHelpers.objectWithoutProperties(options, ["params", "uriParams"]);
+      return axiosInstance(babelHelpers.objectSpread({
         method: 'patch',
-        url: _this.uri || _this.uriFunction(_objectSpread({}, uriParams)),
+        url: _this.uri || _this.uriFunction(babelHelpers.objectSpread({}, uriParams)),
         data: payload,
         params: params,
         responseType: 'json'
       }, endpointOptions));
     });
-
-    _defineProperty(this, "delete", function () {
+    babelHelpers.defineProperty(this, "delete", function () {
       var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-
       var _options$params5 = options.params,
           params = _options$params5 === void 0 ? {} : _options$params5,
           _options$uriParams5 = options.uriParams,
           uriParams = _options$uriParams5 === void 0 ? {} : _options$uriParams5,
-          endpointOptions = _objectWithoutProperties(options, ["params", "uriParams"]);
-
-      return axiosInstance(_objectSpread({
+          endpointOptions = babelHelpers.objectWithoutProperties(options, ["params", "uriParams"]);
+      return axiosInstance(babelHelpers.objectSpread({
         method: 'delete',
-        url: _this.uri || _this.uriFunction(_objectSpread({}, uriParams)),
+        url: _this.uri || _this.uriFunction(babelHelpers.objectSpread({}, uriParams)),
         params: params,
         responseType: 'json'
       }, endpointOptions));
@@ -185,7 +96,7 @@ function EndpointFactory(axiosInstance) {
     }
 
     this.endpointOptions = _endpointOptions;
-  };
+  }, _temp;
 }
 
 var Endpoint = EndpointFactory(axios);
